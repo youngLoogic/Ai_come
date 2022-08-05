15fe62e52ebab532a834130285579c21f3f81a5e
Gabriel Miranda <siilva29@gmail.com>
2022-08-05 15:41:53 -0300

    aicome

--- delivery/forms.py
+++ delivery/forms.py
@@ -1,19 +1,19 @@
 from pyexpat import model
 from django.forms import ModelForm
-from .models import Restaurante # Pedido, ItemPedido, Produto
+from .models import Restaurante , Pedido, ItemPedido, Produto
 
 class RestauranteForm(ModelForm):
     model = Restaurante
     fields = '__all__'
 
-# class PedidoForm(ModelForm):
-#     model = Pedido
-#     fields = '__all__'
+class PedidoForm(ModelForm):
+     model = Pedido
+     fields = '__all__'
 
-# class ItemPedidoForm(ModelForm):
-#     model = ItemPedido
-#     fields = '__all__'
+class ItemPedidoForm(ModelForm):
+     model = ItemPedido     
+     fields = '__all__'
 
-# class ProdutoForm(ModelForm):
-#     model = Produto
-#     fields = '__all__'
+class ProdutoForm(ModelForm):
+     model = Produto
+     fields = '__all__'
